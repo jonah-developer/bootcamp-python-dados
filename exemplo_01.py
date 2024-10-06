705ed820e8cf2ea2c738f4835ad197b1175ae4f0
Paco <pacodeoliveirasaavedra@gmail.com>
2024-10-06 18:16:51 -0300

    passando arquivos da aula01 pra diretorio geral de bootcamp

---
+++ exemplo_01.py
@@ -0,0 +1,3 @@
+# Crie programam que o usuário digite o seu nome e retorne o numero de letras nele
+
+print(len(input("Digite o seu nome: ")))
---
+++ exemplo_02.py
@@ -0,0 +1,21 @@
+# Criar um programa aonde o usuário digite dois valores e apareça a soma
+
+# print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
+
+
+# Tipos de dados (Tipos Primitivos)
+
+numero = 3 # int
+print(type(numero))  # type roda o "tipo" doque for puxado
+# numer_decimal = 4.78 # float
+# nome_de_usuario = "Luciano" # string
+# verdadeiro = True # Bool
+
+# Em python tudo é objeto e tudo é estrutura de dados
+
+# Quando eu falo que o objeto é uma "class" é pq ele pertence 
+# a um grupo
+
+# EM VARIAVEL NADA NUNCA MAIUSCULA,SEMPRE MINUSCULO E COM _
+
+# SOMENTE CONSTANTE COM LETRA MAISUCULA
